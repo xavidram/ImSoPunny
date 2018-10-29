36f21701a173601629ff4b543dc5c074b84769a2
Xavid Ramirez <xavidramirez@AADM08640.local>
2018-10-29 14:59:35 -0500

    set up authentication server side

---
+++ ImSoPunny/Controllers/UsersController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using ImSoPunny.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ImSoPunny.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IConfiguration _configuration;
+
+        public UsersController(
+            UserManager<AppUser> userManager,
+            SignInManager<AppUser> signInManager,
+            IConfiguration configuration
+        )
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public async Task<object> Login([FromBody] CredentialsModel cred)
+        {
+            var result = await _signInManager.PasswordSignInAsync(cred.Email, cred.Password, false, false);
+            if (result.Succeeded)
+            {
+                var appUser = _userManager.Users.SingleOrDefault(r => r.Email == cred.Email);
+                return await GenerateJwtToken(cred.Email, appUser);
+            }
+            throw new ApplicationException("INVALID_LOGIN_ATTEMPT");
+        }
+
+        [HttpPost]
+        public async Task<object> Register([FromBody] CredentialsModel cred)
+        {
+            var user = new AppUser
+            {
+                Email = cred.Email,
+                UserName = cred.Email
+            };
+            var result = await _userManager.CreateAsync(user, cred.Password);
+
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false);
+                return await GenerateJwtToken(cred.Email, user);
+            }
+
+            throw new ApplicationException("UNKNOWN_ERROR");
+        }
+
+        private async Task<object> GenerateJwtToken(string email, AppUser user)
+        {
+            var claims = new List<Claim> {
+                new Claim (JwtRegisteredClaimNames.Sub, email),
+                new Claim (JwtRegisteredClaimNames.Jti, Guid.NewGuid ().ToString ()),
+                new Claim (ClaimTypes.NameIdentifier, user.Id)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JwtKey"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var expires = DateTime.Now.AddDays(Convert.ToDouble(_configuration["JwtExpireDays"]));
+
+            var token = new JwtSecurityToken(
+                _configuration["JwtIssuer"],
+                _configuration["JwtIssuer"],
+                claims,
+                expires: expires,
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
--- ImSoPunny/Models/AppUser.cs
+++ ImSoPunny/Models/AppUser.cs
@@ -4,21 +4,21 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ImSoPunny.Models
 {
-	public class AppUser : IdentityUser
-	{
+    public class AppUser : IdentityUser
+    {
 
-		public AppUser()
-		{
-			Puns = new HashSet<Pun>();
-		}
-		public ICollection<Pun> Puns { get; set; }
-	}
+        public AppUser()
+        {
+            Puns = new HashSet<Pun>();
+        }
+        public ICollection<Pun> Puns { get; set; }
+    }
 
-	public class CredentialsModel
-	{
-		[Required]
-		public string Email { get; set; }
-		[Required]
-		public string Password { get; set; }
-	}
+    public class CredentialsModel
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
 }
